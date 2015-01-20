8c9b6c6ada202c157f400c9ee603cbf9e225f2a9
Sunil Nayak <sunil.nayak133@gmail.com>
2015-01-20 14:17:26 +0530

    modified:   plugins/android.json 	new file:   plugins/org.apache.cordova.plugin.sms/.fetch.json 	new file:   plugins/org.apache.cordova.plugin.sms/LICENSE 	new file:   plugins/org.apache.cordova.plugin.sms/plugin.xml 	new file:   plugins/org.apache.cordova.plugin.sms/readme.md 	new file:   plugins/org.apache.cordova.plugin.sms/src/android/Sms.java 	new file:   plugins/org.apache.cordova.plugin.sms/src/ios/Sms.h 	new file:   plugins/org.apache.cordova.plugin.sms/src/ios/Sms.m 	new file:   plugins/org.apache.cordova.plugin.sms/src/wp8/Sms.cs 	new file:   plugins/org.apache.cordova.plugin.sms/www/sms.js 	modified:   www/index.html 	modified:   www/js/index.js 	new file:   www/js/shake.js 	new file:   www/js/shake/.gitignore 	new file:   www/js/shake/LICENSE.md 	new file:   www/js/shake/README.md 	new file:   www/js/shake/bower.json 	new file:   www/js/shake/index.html 	new file:   www/js/shake/shake.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,6 @@
 
+var AppSettings = new Object();
+
 var app = {
     SOME_CONSTANTS : false,  // some constant
 
@@ -6,6 +8,7 @@ var app = {
         console.log("console log init");
         this.bindEvents();
         this.initFastClick();
+        
     },
     
     bindEvents: function() {
@@ -17,11 +20,51 @@ var app = {
         }, false);
     },
 
+    startSequence: function() {
+    alert("sending sms");
+        var intent = '';
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send("+917760118257", "testing WoSApp", intent, success, error); //"9980792858"
+    alert("sms sent");
+    },
+
 
     onDeviceReady: function() {
         console.log("device ready, start making you custom calls!");
-
+		
+		checkFirstRun();
         //Moar code
 
     }
+
+   
 };
+
+function checkFirstRun()
+{
+	if(typeof( window.localStorage.WoSApp) == "undefined")
+	{
+		//go to config page
+		window.location= "#firstrun-page";
+		//populate details into settings var
+		
+		window.localStorage.setItem("WoSApp",AppSettings);
+	}
+}
+
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+
+    //put your own code here etc.
+    alert('shake!');
+}
